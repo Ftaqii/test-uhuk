744481f46acac52f9b21f5d8dad17a058544432f
ftaqii <taqiifalsafi@gmail.com>
2021-09-26 00:07:32 +0700

    botwa v1

---
+++ message/help.js
@@ -0,0 +1,824 @@
+exports.rafaGanz2 = (prefix) => {
+    return `
+*── 「 KERANG - MENU」 ──*
+
+1. *#kapankah*
+Mengambil jawaban random dari bot.
+Aliases: -
+Usage: *#kapankah* text
+
+2. *#bisakah*
+Mengambil jawaban random dari bot.
+Aliases: -
+Usage: *#bisakah* text
+`
+}
+exports.rafaGanz1 = (prefix) => {
+    return `
+*── 「 MULTI-SESSION MENU 」 ──*
+
+1. *#jadibot*
+JadiBot Sementara.
+Aliases: -
+Usage: *#jadibot* kode-base64
+
+2. *#listbot*
+Menampilkan user yang sedang jadibot.
+Aliaases: -
+Usage: *#artimimpi* tema_mimpi
+
+3. *#stopjadibot*
+Cancel Bot Sementara.
+Aliases: -
+Usage: *#stopjadibot*
+
+4. *#getcode*
+Ambil Session mu.
+Aliases: -
+Usage: *#getcode*
+Note: Hanya untuk yg jadibot.
+`
+}
+exports.rafaGanz = (prefix) => {
+    return `
+
+*── 「 PREMIUM 」 ──*
+Halo premium users ヽ(・∀・)ﾉ!
+
+1. *#tiktoknowm*
+Download Tiktok video tanpa WM.
+Aliases: *tktoknowm*
+Usage: *#tiktoknowm* link_video
+
+2. *#stickerwm*
+Buat stiker dengan WM.
+Aliases: *stcwm*
+Usage: Kirim gambar dengan caption *#stickerwm* pack_name | author_name atau reply gambar dengan caption *#stickerwm* pack_name | author_name.
+
+3. *#takestick*
+Merubah watermark sticker.
+Aliases: -
+Usage: Reply stiker dengan caption *#takestick* pack_name | author_name
+
+4. *#asupan*
+Asupan video cewek-cewek.
+Aliases: -
+Usage: *#asupan*
+
+5. *#addcmd*
+Custom add cmd untuk sticker.
+Aliases: -
+Usage: reply sticker dengan command *#addcmd* cmdnya
+
+6. *#delcmd*
+Custom delete cmd untuk sticker.
+Aliases: -
+Usage: reply sticker dengan command *#delcmd* cmdnya
+`
+}
+exports.gameMenu = (prefix) => {
+    return `
+
+*── 「 GAME - MENU 」 ──*
+ 
+1. *#slot*
+Permainan User
+Aliases : -
+Usage : Slot
+
+2.️ *#limitgame*
+Limit User
+Aliases : -
+Usage : *#limitgame*
+
+3.️ *#gelud @tag*
+gelud User
+Aliases : -
+Usage : *#gelud @tag user*
+
+4.️ *#tictactoe @tag*
+Game User
+Aliases : -
+Usage :  *#tictactoe @tag*
+
+5. *#siapaaku*
+Permainan User
+Aliases : -
+Usage : *#siapaaku*
+
+6.️ *#family100*
+Permainan User
+Aliases : -
+Usage : *#family100*
+
+7.️ *#kuismath*
+Permainan User
+Aliases : -
+Usage : *#kuismath*
+
+8. *#asahotak*
+Permainan User
+Aliases : -
+Usage : *#asahotak*
+
+9.️ *#tebaklirik*
+Permainan User
+Aliases : -
+Usage : *#tebaklirik*
+
+10.️ *#tebaklagu*
+Permainan User
+Aliases : -
+Usage : *#tebaklagu*
+
+11.️ *#tebakkata*
+Permainan User
+Aliases : -
+Usage : *#tebakkata*
+
+12.️ *#susunkata*
+Permainan User
+Aliases : -
+Usage : *#susunkata*
+
+13. *#kimiakuis*
+Permainan User
+Aliases : -
+Usage : *#kimiakuis*
+
+14.️ *#caklontong*
+Permainan User
+Aliases : -
+Usage : *#caklontong*
+
+15.️ *#tebakjenaka*
+Permainan User
+Aliases : -
+Usage : *#tebakjenaka*
+
+16.️ *#tebakanime*
+Permainan User
+Aliases : -
+Usage : *#tebakanime*
+
+17.️ *#tebaktebakan*
+Permainan User
+Aliases : -
+Usage : *#tebaktebakan*
+
+19.️ *#tebakgambar*
+Permainan User
+Aliases : -
+Usage : *#tebakgambar*
+
+20. *#tebakbendera*
+Permainan User
+Aliases : -
+Usage : *#tebakbendera*
+
+21.️ *#suit batu/kertas/gunting*
+Permainan User
+Aliases : -
+Usage : *#suit batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+
+*── 「 FUN - MENU 」 ──*
+
+1. *#mining*
+-Game User
+Aliases : -
+Usage : *#mining*
+
+2.️ *#togel*
+-Game User
+Aliases : -
+Usage : *#togel*
+
+3.️ *#cekwatak*
+-Game User
+Aliases : -
+Usage : *#cekwatak*
+
+4. *#cekmati*
+-Game User
+Aliases : -
+Usage : *#cekmati*
+
+5. *#wangy*
+-Game User
+Aliases : -
+Usage : *#wangy*
+
+6.️ *#citacita*
+-Game User
+Aliases : -
+Usage : *#citacita*
+
+7. *#toxic*
+-Game User
+Aliases : -
+Usage : *#toxic*
+
+8.️ *#truth*
+-Game User
+Aliases : -
+Usage : *#truth*
+
+9.️ *#dare*
+-Game User
+Aliases : -
+Usage : *#dare*
+
+10.️ *#apakah*
+-Game User
+Aliases : -
+Usage : *#apakah*
+
+11.️ *#bisakah*
+-Game User
+Aliases : -
+Usage : *#bisakah*
+
+12. *#kapankah*
+-Game User
+Aliases : -
+Usage : *#kapankah*
+
+13. *#rate*
+-Game User
+Aliases : -
+Usage : *#rate*
+
+14. *#jadian*
+-Game User
+Aliases : -
+Usage : *#jadian*
+
+15. *#cantik*
+-Game User
+Aliases : -
+Usage : *#cantik*
+
+16.️ *#ganteng*
+-Game User
+Aliases : -
+Usage : *#ganteng*
+
+17.️ *#beban*
+-Game User
+Aliases : -
+Usage : *#beban*
+
+18. *#babi*
+-Game User
+Aliases : -
+Usage : *#babi*
+
+19. *#cekganteng*
+-Game User
+Aliases : -
+Usage : *#cekganteng*
+
+20️ *#cekcantik
+-Game User
+Aliases : -
+Usage : *#cekcantik*
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+*── 「 OTHER - MENU」 ──*
+
+1. *${prefix}shope*
+*${prefix}shope*
+Aliases:
+Usage: Mencari product.
+
+2. *${prefix}playstore*
+Aliases: -
+Usage: Mencari game dll.
+
+3. *${prefix}ssweb*
+*${prefix}ssweb*
+Aliases: -
+Usage: Screnshot web
+
+4. *${prefix}google*
+*${prefix}google*
+Aliases: -
+Usage: -
+
+5. *${prefix}image*
+*${prefix}image*
+Aliases: -
+Usage: -
+
+6. *${prefix}pinterest*
+*${prefix}pinterest*
+Aliases: -
+Usage: Mencari sesuatu🤤
+
+7. *${prefix}nulis*
+*${prefix}nulis*
+Aliases: - 
+Usage: Nulis Manual!!
+
+8. *${prefix}iguser*
+*${prefix}iguser*
+Aliases: -
+Usage: -
+
+9. *${prefix}igstalk*
+*${prefix}igstalk*
+Aliases: -
+Usage: -
+
+10. *${prefix}tiktokstalk*
+*${prefix}tiktokstalk*
+Aliases: -
+Usage: -
+
+11. *${prefix}img2url*
+*${prefix}img2url*
+Aliases: -
+Usage: Sebuah foto yang menjadi link.
+
+12. *${prefix}ytsearch*
+*${prefix}ytsearch*
+Aliases: -
+Usage: Mencari audio dan lain".
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+*── 「 STICKER - MENU 」 ──*
+
+1. *${prefix}dadu*
+*${prefix}dadu*
+Aliases: -
+Usage: -
+
+2. *${prefix}doge*
+*${prefix}doge*
+Aliases: -
+Usage: -
+
+3. *${prefix}toimg*
+*${prefix}toimg*
+Aliases: -
+Usage: Membuat stiker menjadi image.
+
+4. *${prefix}patrick*
+*${prefix}patrick*
+Aliases: -
+Usage: mengrim stiker patrick
+
+5. *${prefix}garwgura*
+*${prefix}garwgura*
+Aliases: -
+Usage: -
+
+6. *${prefix}attp*
+*${prefix}attp*
+Aliases: -
+Usage: Membuat sticker text warna warni.
+
+7. *${prefix}stickeranime*
+*${prefix}stickeranime*
+Aliases: -
+Usage: Mengirim stickeranime.
+
+8. *${prefix}semoji*
+*${prefix}semoji*
+Aliases: -
+Usage: Membuat emoji menjadi sticker.
+
+9. *${prefix}sticker*
+*${prefix}sticker*
+Aliases: -
+Usage: Membuat foto menjadi sticker.
+
+10. *${prefix}smeme*
+*${prefix}smeme*
+Aliases: -
+Usage: Menambahkan text di stiker
+
+11. *${prefix}swm*
+*${prefix}swm*
+Aliases: -
+Usage: Membuat sticker dengan watermark.
+
+12. *${prefix}take*
+*${prefix}take*
+Aliases: -
+Usage: Ambil sticker orang lain dengan watermark.
+
+13. *${prefix}tovideo*
+*${prefix}tovideo*
+Aliases: -
+Usage: Mengubah sticker gif menjadi video.
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+
+*── 「 WIBU - MENU 」 ──*
+
+1. *#loli*
+search bot world images
+Aliases: -
+Usage: *#loli*
+
+2. *#manga*
+search bot world images
+Aliases: -
+Usage: *#manga*
+
+3.️ *#anime*
+search bot world images
+Aliases: -
+Usage:
+
+4.️ *#lolivideo*
+search bot world images
+Aliases: -
+Usage: 
+
+5.️ *#husbu*
+search bot world images
+Aliases: -
+Usage: *#husbu*
+
+6. *#waifu*
+search bot world images
+Aliases: -
+Usage: *#waifu*
+
+7.️ *#milf*
+search bot world images
+Aliases: -
+Usage: *#milf*
+
+8.️ *#neko*
+search bot world images
+Aliases: -
+Usage: *#neko*
+
+9.️ *#kanna*
+search bot world images
+Aliases: -
+Usage: *#kanna*
+
+10.️ *#sagiri*
+search bot world images
+Aliases: -
+Usage: *#sagiri*
+
+11.️ *#hentai
+search bot world images
+Aliases: -
+Usage: *#hentai*
+
+12.️ *#cosplay*
+search bot world images
+Aliases: -
+Usage: *#cosplay*
+
+13.️ *#wallnime*
+search bot world images
+Aliases: -
+Usage: *#wallnime*
+
+14.️ *#kusonime*
+search bot world images
+Aliases: -
+Usage: *#kusonime*
+
+15. *#megumin*
+search bot world images
+Aliases: -
+Usage: *#megumin*
+
+16. *#otakudesu*
+search bot world images
+Aliases: -
+Usage: *#otakudesu*
+
+19.️ *#doujindesu*
+search bot world images
+Aliases: -
+Usage: *#doujindesu*
+
+20.️ *#storyanime*
+search bot world images
+Aliases: -
+Usage: *#storyanime*
+
+21. *#nakanomiku*
+search bot world images
+Aliases: -
+Usage: *#nakanomiku*
+
+22.️ *#nakanonino*
+search bot world images
+Aliases: -
+Usage: *#nakanonino*
+
+23.️ *#nakanoitsuki*
+search bot world images
+Aliases: -
+Usage: *#nakanoitsuki*
+
+24.️ *#otakuongoing*
+search bot world images
+Aliases: -
+Usage: *#otakuongoing*
+
+25.️ *#nhentai *code*
+search bot world images
+Aliases: -
+Usage: *#nhentai *code*
+
+26.️ *#nekopoi *link*
+search bot world images
+Aliases: -
+Usage: *#nekopoi *link*
+
+27.️ *#nekopoi3d*
+search bot world images
+Aliases: -
+Usage: *#nekopoi3d*
+
+28. *#nekopoicosplay*
+search bot world images
+Aliases: -
+Usage: *#nekopoicosplay*
+
+
+29.️ *#nekopoisearch* 
+search bot world images
+Aliases: -
+Usage: *#nekopoisearch*
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+┌──「 OWNER-MENU 」
+│
+├ ❏ ${prefix}bc *teks*
+├ ❏ ${prefix}term
+├ ❏ ${prefix}eval
+├ ❏ ${prefix}reset
+├ ❏ ${prefix}clearall
+├ ❏ ${prefix}leaveall
+├ ❏ ${prefix}join *link gc*
+├ ❏ ${prefix}shutdown
+├ ❏ ${prefix}getquoted
+├ ❏ ${prefix}addupdate *fiturnya*
+├ ❏ ${prefix}exif *nama|author*
+├ ❏ ${prefix}sewa add/del *waktunya*
+├ ❏ ${prefix}premium add @tag|nomor
+├ ❏ ${prefix}premium del @tag|nomor
+│
+└──「 *NAKANO BOT* 」
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+*── 「 MODERATION GROUP 」 ──*
+
+1. *!add*
+Menambah user ke dalam group.
+Aliases: -
+Usage: *!add* 628xxxxxxxxxx
+
+2. *!kick*
+Mengeluarkan member dari grup.
+Aliases: -
+Usage: *!kick* @member1
+
+3. *!promote*
+Promote member menjadi admin.
+Aliases: -
+Usage: *!promote* @member1
+
+4. *!demote*
+Demote member dari admin.
+Aliases: -
+Usage: *!demote* @member1
+
+5. *!leave*
+Bot akan meninggalkan grup.
+Aliases: -
+Usage: *!leave*
+
+6. *!tagall*
+Mention semua member group.
+Aliases: *everyone*
+Usage: *!tagall*
+
+7. *group*
+Mematikan/menyalakan group 
+Aliases: -
+Usage: *grup* enable/disable
+
+8. *!groupicon*
+Mengganti icon grup.
+Aliases: -
+Usage: Kirim gambar dengan caption *!groupicon* atau reply gambar dengan caption *!groupicon*.
+
+9. *!antilink*
+Mematikan/menyalakan fitur anti-group link.
+Aliases: -
+Usage: *!antilink* enable/disable
+
+10. *!welcome*
+Mematikan/menyalakan fitur welcome di grup agar menyambut setiap kedatangan member.
+Aliases: -
+Usage: *!welcome* enable/disable
+
+13. *!setdecs*
+Mengubah tampilan deskripsi.
+Aliases: -
+Usage: *!setdecs*
+
+14. *!linkgroup*
+Cek link invite group.
+Aliases: -
+Usage: *!linkgroup*
+
+15. *!listonline*
+Memberikan/tag yang online di grup.
+Aliases: -
+Usage: *!listonline*
+
+16. *!mute*
+Membisukan bot di grup.
+Aliases: -
+Usage: *!mute* enable/disable
+
+17. *!ceksewa*
+Melihat waktu sewa bot.
+Aliases: -
+Usage: Melihat Waktu Sewa Anda
+
+18. *!hidetag*
+Ngetag semua orang.
+Aliases: -
+Usage: -
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+
+*── 「 DOWNLOADER 」 ──*
+
+1. *#facebook*
+Download Facebook video.
+Aliases: *fb*
+Usage: *#facebook* link_video
+
+2. *#tiktok*
+Mendownload video TikTok.
+Aliases: -
+Usage: *#tiktok* link_video
+
+3. *#twitter*
+Download Twitter media.
+Aliases: *twt*
+Usage: *#twiter* link
+
+4. *#tiktokmusic*
+Download source music dari tiktokvideo.
+Aliases: -
+Usage: *#tiktokmusic* link_video
+
+5. *#igdl*
+Download video dari Source Instagram.
+Aliases: *instagramdl*, *ig*
+Usage: *#ig* link_video
+
+6. *#mediafire*
+Info dan direct link mediafire.
+Aliases: -
+Usage: *#mediafire* link
+
+7. *#soundcloud*
+Download soundcloud music.
+Aliases: -
+Usage: *#souncloud* link_music   
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+ Syarat & Ketentuan ApiccaBot
+
+• Nomor ApiccaBot resmi hanya ada di pricing.herokuapp.com
+• ApiccaBot *hanya menyimpan nomor anda* di dalam database sebagai nomor user
+• ApiccaBot *tidak pernah meminta informasi pribadi* anda seperti alamat rumah, asal daerah dan lain lain.
+• Dilarang melakukan spam terhadap bot
+• Dilarang menelpon bot
+• Harap menggunakan fitur bot dengan bijak
+• ApiccaBot tidak menyimpan foto, video, atau media apapun yang anda kirimkan
+• ApiccaBot *tidak bertanggung jawab atas fitur apapun yang anda gunakan*
+• Apabila menemukan bug, error, ataupun request fitur harap hubungi devloper
+• ApiccaBot *berhak untuk memblokir* atau melakukan ban terhadap user dengan alasan maupun tanpa alasan
+
+© By Rafasurya
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+*── 「 INFO 」 ──*
+
+1. *!update
+*!update*
+Aliases: -
+Usage: -
+
+2. *!level*
+*!level* cek level
+Aliases: -
+Usage: Mengeck level anda
+
+3. *!rules*
+*!rules* Peraturan bot.
+Aliases: -
+Usage: Peraturan dalam menggunakan bot.
+
+4. *!profile*
+*!profile* 
+Aliases: -
+Usage: cek foto profile bio.
+
+5. *!waktu*
+*!waktu*
+Aliases: -
+Usage: Melihat jam indonesia wib.wita.wit
+
+6. *!botstat*
+*!botstat*
+Aliases: -
+Usage: Melihat Status bot.
+
+7. *!sewabot*
+*!sewabot*
+Aliases: -
+Usage: Melihat harga sewabot.
+
+8. *!listsewa*
+*!listsewa*
+Aliases: -
+Usage:
+
+9. *${prefix}owner*
+*${prefix}owner*
+Aliases: -
+Usage: Melihat nope pemilik bot.
+
+10. *${prefix}ping*
+*${prefix}ping*
+Aliases: -
+Usage: Melihat kecepatan jaringan bot.
+
+11. *${prefix}runtime*
+*${prefix}runtime*
+Aliases: -
+Usage: -
+
+12. *${prefix}donasi*
+*${prefix}donasi*
+Aliases: -
+Usage: -
+
+13. *${prefix}leadebord*
+*${prefix}leaderboard*
+Aliases: -
+Usage: mengecek rank yang ad di group.
+
+14. *${prefix}cekprem*
+*${prefix}cekprem*
+Aliases: -
+Usage: Melihat waktu premium anda.
+
+15. *${prefix}listpremiun*
+*${prefix}listpremium*
+Aliases: -
+Usage: Melihat user premium.
+
+16. *${prefix}sourcecode*
+*${prefix}sourcecode*
+Aliases: -
+Usage: -
+
+17. *${prefix}bugreport*
+*${prefix}bugreport*
+Aliases: -
+Usage: jika ada fitur yg eror report.
+`
+} 
